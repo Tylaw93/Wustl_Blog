b1e754c8d40497165617c5dba3b8d083a457f622
Tyler Lawson <tylaw93@yahoo.com>
2021-10-08 15:14:58 -0500

    Updating

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+// 2 Establish Server Port
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
 const sess = {
@@ -25,9 +27,11 @@ const sess = {
 
 app.use(session(sess));
 
+// Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+// 3 Use express and Sequelize to access the database through the routes (through controllers/index.js) (see config/connection.js)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
